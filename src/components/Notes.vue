40abde0ed97db6e11533e26395908f478bc4487f
Sosna123 <olek.arez@gmail.com>
2023-08-18 11:05:04 +0200

    POST fetching works

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -3,18 +3,17 @@
         <h1>Notes:</h1>
         <input type="text" v-model="currentNote">
         <button @click="addNote">Submit</button>
-        <div>
-            <div v-for="note in notes" :key="note">
+        <ul>
+            <li v-for="note in notes" :key="note">
                 <p>{{ note.content }}</p>
                 <button @click="removeNote(note)">remove</button>
-            </div>
-        </div>
-        
+            </li>
+        </ul>
     </div>   
 </template>
 
 <script>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 export default {
     name: 'Home',
@@ -23,17 +22,33 @@ export default {
         let notes = ref([]);
 
         // fetche
-
         let fetchData = async () => {
             const response = await fetch('http://localhost:3000')
             const data = response.json()
             return data;
         }
 
+        let fetchDataPost = async (data) => {
+            await fetch('http://localhost:3000', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            })
+        }
+
+        // funkcje
         let addNote = () => {
             if(currentNote.value != '') {
-                notes.value.push({user: 'sosna', content: currentNote.value})
-                currentNote.value = ''
+                let pushNote = {user: 'sosna', content: currentNote.value};
+                fetchDataPost(pushNote).then(() => {
+                    notes.value.push(pushNote);
+                    currentNote.value = '';
+                    pushNote = '';
+                    notes.value = [];
+                    displayData();
+                });
             }
         }
 
@@ -57,7 +72,7 @@ export default {
         }
 
         // wczytywanie notek z api
-        displayData();
+        onMounted(() => displayData())
 
         return{
             // zmienne
