95337ea9ebeda8a63fc5dd198c98bd37d9e4a648
Sosna123 <olek.arez@gmail.com>
2023-09-03 22:00:33 +0200

    CSS

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -11,7 +11,7 @@
             <h1>Your notes:</h1>
             <li v-for="note in notes" :key="note">
                 <p class="noteText">{{ note.content }}</p>
-                <button @click="removeNote(note)" class="material-symbols-outlined">delete</button>
+                <button class="noteRemove material-symbols-outlined" @click="removeNote(note)">delete</button>
             </li>
         </ul>
     </div>
@@ -106,37 +106,83 @@ export default {
 /* divs */
 .Notes{
     display: inline;
+    color: white;
 }
 
 .Notes div{
-    background-color: grey;
-    padding: 20px;
+    background-color: rgba(255, 255, 255, 0.15);
+    padding: 1.25em;
     border-radius: 20px;
-    margin: 15px 20px;
+    margin: 0.9375em 1.25em;
 }
 
+ul{
+    background-color: rgb(84, 87, 92);
+    padding: 0.625em;
+    margin: 0 1.25em;
+    border-radius: 20px;
+    padding: 1.25em;
+}
 
 /* content */
+
+    /* note top */
+
 .Notes div > h1{
     margin-top: 0;
 }
 
-ul > li > * {
-    display: inline-block;
-}
-
-.noteText{
-    font-size: 20px;
-    margin-right: 10px;
-    padding: 10px;
-}
-
 .addInput{
-    padding: 5px;
+    padding: 0.625em;
+    border-radius: 10px;
+    color: black;
+    background: white;
+    border: black 2px solid;
+}
+
+.addInput::placeholder{
+    color: black;
+    opacity: 1;
 }
 
 .addButton{
-    padding: 5px;
+    padding: 0.625em;
+    border-radius: 10px;
+    margin-left: 0.3125em;
+    background: white;
+    border: black 2px solid;
+    font-family: "Montserrat", sans-serif;
+}
+
+.addButton:hover{
+    background: rgb(199, 199, 199);
+}
+
+    /* note list */
+
+.noteRemove{
+    background: crimson;
+    border: black 2px solid;
+    border-radius: 15px;
+    padding: 0.3125em;
+    text-align: center;
+}
+
+.noteRemove:hover{
+    background: rgb(164, 35, 61);
+}
+
+.noteText{
+    font-size: 1.4375em;
+    padding: 0.625em;
+}
+
+ul > li{
+    list-style: none;
+}
+
+ul > li > *{
+    display: inline-block;
 }
 
 /* google icons */
